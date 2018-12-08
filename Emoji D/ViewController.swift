8536aee496282deef57953a79f9b3c236c3ce551
ck <ck@ansonde-macbook-pro.local>
2018-12-08 15:12:31 +0800

    Initial Commit

---
+++ Emoji D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji D
+//
+//  Created by ck on 8/12/18.
+//  Copyright © 2018 ck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
